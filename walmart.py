b03792498b6516ee53433532f7c300025d83094f
prajwal.s <prajwal.s@hpe.com>
2018-03-14 16:22:48 +0530

    folder added to play with fork

--- walmart.py
+++
@@ -1,6 +0,0 @@
-groceries = {'milk','butter','groundnut','seasame','noodles','butter'}
-print(groceries)
-if 'milk' in groceries:
-    print("you purchased milk")
-else:
-    print("it is not there in list")
